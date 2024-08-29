8b79b96f4b81a9335ad8c78ebaaf45c04d9597ef
terious <garden940316@gmail.com>
2024-08-29 20:39:42 +0900

    print

---
+++ variablesAndFunctions/helloworld.py
@@ -0,0 +1,7 @@
+# Python은 String을 감쌀 때 "", '' 중 아무거나 사용하면 된다.
+print("hello wolrd")
+print('hello wolrd')
+
+age = 12
+print("Hello I'm", age, "years old!")
+print("Hello I'm     ", age, "    years old too!")
