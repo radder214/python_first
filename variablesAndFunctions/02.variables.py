703951fe23cfa6507c58f4aac4b706878bec58c1
terious <garden940316@gmail.com>
2024-09-01 16:39:12 +0900

    variable, boolean, String

---
+++ variablesAndFunctions/02.variables.py
@@ -0,0 +1,15 @@
+# Python은 var, let, const 같은 키워드 자체가 없다.
+a = 2
+b = 3
+c = a + b
+print("c :", c)
+
+c = 1
+print("c :", c)
+
+my_age = 24
+print(my_age)
+
+myAge = 28
+print(myAge)
+print(my_age + myAge)
---
+++ variablesAndFunctions/03.booleans_strings.py
@@ -0,0 +1,21 @@
+# ======= String =======
+# Python 은 String 에 “double quotes”, ‘single quotes’ 아무거나 사용해도 상관없다.
+my_name = "Jose"
+print(my_name)
+
+my_name2 = 'Bath'
+print(my_name2)
+
+# ======= Boolean =======
+# Python에서 Boolean 은 반드시 True, False 로 작성해야 한다.
+bool1 = True
+bool2 = False
+
+print(bool1)
+print(bool2)
+
+# 아래와 같이 작성하면 error 발생
+# NameError: name 'true' is not defined. Did you mean: 'True'? 
+# bool1 = true
+# NameError: name 'false' is not defined. Did you mean: 'False'?
+# bool2 = false
