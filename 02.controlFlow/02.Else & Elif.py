1c5a95dbb33bee79cf1d115a69cfd11f04620a09
terious <garden940316@gmail.com>
2024-09-02 19:35:16 +0900

    else, elif

---
+++ 02.controlFlow/02.Else & Elif.py
@@ -0,0 +1,15 @@
+# else
+password_Correct = True
+if password_Correct:
+    print("Show me the money")
+else:
+    print("Go the Police")
+
+# elif
+winner = 10
+if winner > 10:
+    print("Winner point is greater than 10")
+elif winner < 10:
+    print("Winner point is less than 10")
+else:
+    print("Winner point is 10")
