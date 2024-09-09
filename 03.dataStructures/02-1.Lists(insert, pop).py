4fa44d26351ba2d8fb277cd17e0691facf21ac93
terious <garden940316@gmail.com>
2024-09-09 18:58:26 +0900

    comment

--- 03.dataStructures/02-1.Lists(insert, pop).py
+++ 03.dataStructures/02-1.Lists(insert, pop).py
@@ -1,9 +1,9 @@
 list = [1, 2, 3, 4, 5]
 
 # list의 특정 index에 element 삽입
-list.insert(2, 99)
+list.insert(2, 99) # 2번 index 99 삽입
 print(list)
 
 # list의 특정 index의 element 삭제
-list.pop(4)
+list.pop(4) # 4번 index element 삭제
 print(list)
