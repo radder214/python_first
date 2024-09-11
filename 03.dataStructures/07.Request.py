4b35e74f742d55d190bc8033266e808744221869
terious <garden940316@gmail.com>
2024-09-11 19:42:11 +0900

    requests package import By PyPI

---
+++ 03.dataStructures/07.Request.py
@@ -0,0 +1,18 @@
+# Python Standard Library 가 아닌 인터넷에서 다운 받은 package import
+# import requests
+
+from requests import get
+
+websites = [
+    "google.com",
+    "airbnb.com",
+    "https://twitter.com",
+    "facebook.com",
+    "https://amazon.com"
+]
+
+for website in websites:
+    if not website.startswith("https://"):
+        print(f"have to fix it ==> {website}")
+        website = f"https://{website}"
+        print(f"fixed it       ==> {website}")
