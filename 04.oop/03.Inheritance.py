c5d655a2e09e87592fa0b717462554b65c2824bf
terious <garden940316@gmail.com>
2024-09-15 23:18:05 +0900

    상속 코드 수정

--- 04.oop/03.Inheritance.py
+++ 04.oop/03.Inheritance.py
@@ -1,4 +1,4 @@
-# >>>>>>>>>>>>>>> 부모 class
+# [부모 class]
 class Dog:
     def __init__(self, name, color, age):
         self.name   = name
@@ -7,25 +7,33 @@ class Dog:
 
     def __str__(self):
         return f"Dog : {self.name}, {self.color}, {self.age}"
-    
+
+    def beforeSleep(self):
+        print(f"{self.name} ready to sleep...")
+
     def sleep(self):
+        self.beforeSleep() # 이런 식으로 class 내 다른 method 호출 가능
         print(f"{self.name} is sleeping...zzz...")
 
-# >>>>>>>>>>>>>>> 자식 class, (Dog) --> Dog class를 상속 받는다.
-class GuardDog(Dog):
+# [자식 class]
+class GuardDog(Dog): # (Dog) --> Dog class 를 상속 받는다. Dog class가 가지고 있는 모든 property 를 상속 받는다.
     def __init__(self, name, color, age):
-        super().__init__(name, color, age) # 부모 class의 생성자 호출
+        # 부모 class의 __init__ method 호출(부모 class의 constructor 호출)
+        # super : 부모 class를 의미(Dog class)
+        super().__init__(name, color, age)
 
     def rrrrr(self):
-        print("stay away...")
+        print("GuardDog bark! bark!")
 
-# >>>>>>>>>>>>>>> 자식 class, (Dog) --> Dog class를 상속 받는다.
-class Puppy(Dog):
+# [자식 class]
+class Puppy(Dog): # (Dog) --> Dog class 를 상속 받는다. Dog class가 가지고 있는 모든 property 를 상속 받는다.
     def __init__(self, name, color, age):
-        super().__init__(name, color, age) # 부모 class의 생성자 호출
+        # 부모 class의 __init__ method 호출(부모 class의 constructor 호출)
+        # super : 부모 class를 의미(Dog class)
+        super().__init__(name, color, age)
 
-    def wolf(self):
-        print("wolf wolf!!!")
+    def woof(self):
+        print("Puppy bark! bark!")
 
 # ################### instance 생성 ###################
 radder = GuardDog(
@@ -39,11 +47,16 @@ nepa = Puppy(
     age   = 9
 )
 
-# ################### instance 출력 ###################
+# ################### GuardDog instance 출력 ###################
+print("=================================")
 print(radder)
 radder.rrrrr()
 radder.sleep()
+print("=================================")
 
+# ################### Puppy instance 출력 ###################
+print("=================================")
 print(nepa)
-nepa.wolf()
+nepa.woof()
 nepa.sleep()
+print("=================================")
